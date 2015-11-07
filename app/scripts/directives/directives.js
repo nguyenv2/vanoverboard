24ff60da1dd484a555394c97d7e04a5408f65bad
Nguyen, Van <Van.Nguyen@washpost.com>
2015-11-07 00:28:02 -0500

    finished directive for blog teasers on the blog.html page

--- app/scripts/directives/directives.js
+++ app/scripts/directives/directives.js
@@ -3,12 +3,16 @@
 angular.module('vangoApp').directive('blogTeaser',function() {
     return {
         restrict: 'E',
-        link: function(scope, elem, attrs, ctrl) {
+        link: function(scope, elem, attrs) {
           console.log('link function...');
-          console.log(attrs);
           scope.blogtitle = attrs.blogtitle;
           scope.pubdate = attrs.pubdate;
         },
-        templateUrl: '/views/templates/blog_teaser.html'
+        //templateUrl: '/views/templates/blog_teaser.html'
+        template:
+          '<div class="row">'+
+            '<div class="col-sm-8">{{blogtitle}}</div>'+
+            '<div class="col-sm-4">{{pubdate}}</div>'+
+          '</div>'
     };
 });
