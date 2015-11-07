9d162104b42128d1dfca2f44ca8dcacb955eb5dd
Van Nguyen <van.nguyen@washpost.com>
2015-11-07 11:07:27 -0500

    more stuff

--- app/scripts/directives/directives.js
+++ app/scripts/directives/directives.js
@@ -3,16 +3,34 @@
 angular.module('vangoApp').directive('blogTeaser',function() {
     return {
         restrict: 'E',
+        transclude: true,
         link: function(scope, elem, attrs) {
           console.log('link function...');
-          scope.blogtitle = attrs.blogtitle;
           scope.pubdate = attrs.pubdate;
         },
         //templateUrl: '/views/templates/blog_teaser.html'
         template:
           '<div class="row">'+
-            '<div class="col-sm-8">{{blogtitle}}</div>'+
-            '<div class="col-sm-4">{{pubdate}}</div>'+
+            '<div class="col-sm-8 blog-title" ng-transclude></div>'+
+            '<div class="col-sm-4 text-right">{{pubdate}}</div>'+
           '</div>'
+    };
+});
+
+angular.module('vangoApp').directive('projectTeaser',function() {
+    return {
+        restrict: 'E',
+        transclude: true,
+        link: function(scope, elem, attrs) {
+            console.log('link function...');
+            scope.projecttitle = attrs.projecttitle;
+            scope.projectdesc = attrs.projectdesc;
+            console.log(attrs);
+            console.log(elem);
+        },
+        //templateUrl: '/views/templates/blog_teaser.html'
+        template:
+        '<div class="project-title">{{projecttitle}}</div>'+
+        '<div class="project-desc" ng-transclude></div>'
     };
 });
