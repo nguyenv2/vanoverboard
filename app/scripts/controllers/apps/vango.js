2007a463941b73cade2190c1a852e464efcf7dbf
Nguyen, Van <Van.Nguyen@washpost.com>
2015-11-09 17:40:10 -0500

    finished directive for blog teasers on the blog.html page

--- app/scripts/controllers/apps/vango.js
+++ app/scripts/controllers/apps/vango.js
@@ -11,49 +11,256 @@ angular.module('vanoverboardApp')
     .controller('VanGoCtrl', ['$scope','$http','bikeshareService',
         function ($scope,$http,bikeshareService) {
 
-            console.log('(VanGoCtrl) starting...');
+          var curDate = new Date();
+
+/*
+          function getTime(zone, success) {
+            var url = 'http://json-time.appspot.com/time.json?tz=' + zone,
+              ud = 'json' + (+new Date());
+            window[ud]= function(o){
+              success && success(new Date(o.datetime));
+            };
+            document.getElementsByTagName('head')[0].appendChild((function(){
+              var s = document.createElement('script');
+              s.type = 'text/javascript';
+              s.src = url + '&callback=' + ud;
+              return s;
+            })());
+          }
+
+          getTime('GMT', function(time){
+            // This is where you do whatever you want with the time:
+            alert(time);
+          });
+          */
 
 
-            bikeshareService.getStationStatus(function(results) {
-                $scope.results = results;
-                console.log('results: ' + results);
-            },function(err){
-                //400 error - bad request
-                $scope.errors=err.data;
-                console.error(err.data);
+
+          $scope.no_buses_running_work = true;
+          $scope.no_buses_running_home = true;
+
+          //Station Codes
+          var station = {
+            mcpherson_square:"C02",
+            court_house:"K01",
+
+            clarendon:"K02",
+
+            largo_town_center:"G05", // eastbound silver
+            new_carrollton:"D13", // eastbound orange
+            vienna:"K08", // westbound orange
+            wiehle:"N06"  // westbound siler
+          }
+
+          $scope.api_key = '4cf4c4ec54fe4d569a1ac414036dac83';
+
+          //$scope.station_code = 'All';
+          var metro={
+            goHome:{
+              url:'https://api.wmata.com/StationPrediction.svc/json/GetPrediction/' + station.mcpherson_square +'?api_key=' + $scope.api_key,
+              lines:[station.vienna, station.wiehle]
+            },
+            goWork:{
+              url:'https://api.wmata.com/StationPrediction.svc/json/GetPrediction/' + station.mcpherson_square +'?api_key=' + $scope.api_key,
+              lines:[station.largo_town_center, station.new_carrollton]
+            }
+          }
+
+          //var goWork = 'https://api.wmata.com/StationPrediction.svc/json/GetPrediction/All?api_key=' + $scope.api_key;
+
+          function metroHome(){
+            // Trains at McPherson heading home
+            $http({
+              method: 'get',
+              url: metro.goHome.url,
+              responseType: "json"
+            }).
+            success(function(data){
+              console.log('Success');
+
+              var trains = data.Trains;
+              var trainsHeadingHome = [];
+
+              for (var x in trains) {
+                for (var y in metro.goHome.lines){
+                  if (metro.goHome.lines[y] === trains[x].DestinationCode){
+                    trainsHeadingHome.push(trains[x]);
+                  }
+                }
+              }
+
+              $scope.trainsHeadingHome = trainsHeadingHome;
+
+            }).
+            error(function(data){
+              console.log('Error: ' + data);
             });
+          }
 
 
+          function metroWork(){
+            // Trains at Court House heading towards work
+            $http({
+              method: 'get',
+              url: metro.goWork.url,
+              responseType: "json"
+            }).
+            success(function(data){
+              console.log('Success');
 
-            $http.get("https://api.wmata.com/StationPrediction.svc/json/GetPrediction/B03&api_key=4cf4c4ec54fe4d569a1ac414036dac83")
-                .success(function(response) {
-                    $console.log(response);
-                });
+              var trains = data.Trains;
+              var trainsHeadingWork = [];
 
+              for (var x in trains) {
+                for (var y in metro.goWork.lines){
+                  if (metro.goWork.lines[y] === trains[x].DestinationCode){
+                    trainsHeadingWork.push(trains[x]);
+                  }
+                }
+              }
 
+              $scope.trainsHeadingWork = trainsHeadingWork;
 
-            $(function() {
-                var params = {
-                    // Request parameters
-                };
-
-                $.ajax({
-                        url: "https://api.wmata.com/StationPrediction.svc/json/GetPrediction/B03&",
-                        beforeSend: function(xhrObj){
-                            // Request headers
-                            xhrObj.setRequestHeader("api_key","4cf4c4ec54fe4d569a1ac414036dac83");
-                        },
-                        type: "GET",
-                        // Request body
-                        data: "{body}",
-                    })
-                    .done(function(data) {
-                        alert("success");
-                    })
-                    .fail(function() {
-                        alert("error");
-                    });
+            }).
+            error(function(data){
+              console.log('Error: ' + data);
             });
+          }
+
+
+
+
+          // 3Y bus arrivals
+
+          //work start - Rt 29 Lee Hwy + Cleveland St	East to McPherson Square	6000701
+          //work end - K St Nw + 15th St Nw	East to McPherson Square	1001221
+
+          //home start - 15th St Nw + I St Nw & K St Nw	West to Harrison Street & Lee Hwy	1001199
+          //home end - Rt 29 Lee Hwy + Calvert St	West to Harrison Street & Lee Hwy	6000705
+          var busstop ={
+            work:{
+              start:'6000701',
+              end:'1001221'
+            },
+            home:{
+              start:'1001199',
+              end:'6000705'
+            }
+          }
+
+          var bus={
+            routeId:'3Y',
+            goHome:{
+              url:'https://api.wmata.com/NextBusService.svc/json/jPredictions?StopID='+ busstop.home.start +'&api_key=' + $scope.api_key
+            },
+            goWork:{
+              url:'https://api.wmata.com/NextBusService.svc/json/jPredictions?StopID='+ busstop.work.start +'&api_key=' + $scope.api_key
+            }
+          }
+
+          function busWork(){
+            // towards McPherson
+            $http({
+              method: 'get',
+              url: bus.goWork.url,
+              responseType: "json"
+            }).
+            success(function(data){
+              var buses = data.Predictions;
+              var busesHeadingWork = [];
+
+              for (var x in buses) {
+                if (bus.routeId === buses[x].RouteID){
+                  // calculate arrival time
+                  var d1 = new Date ();
+                  var d2 = new Date ( d1 );
+                  d2.setMinutes ( d1.getMinutes() + buses[x].Minutes );
+
+                  var localTime = d2.toLocaleTimeString();
+                  var ampm = localTime.split(' ');
+
+                  var time = localTime.split(':');
+                  time[0]+':'+time[1]+' '+ampm[1]
+                  buses[x].ArrivalTime = time[0]+':'+time[1]+' '+ampm[1];
+
+                  busesHeadingWork.push(buses[x]);
+                }
+
+              }
+
+              $scope.busesHeadingWork = busesHeadingWork;
+              if (busesHeadingWork.length > 0){
+                $scope.no_buses_running_work = false;
+              }
+
+            }).
+            error(function(data){
+              console.log('Error: ' + data);
+            });
+          }
+
+          function busHome(){
+            // towards Arlington
+            $http({
+              method: 'get',
+              url: bus.goHome.url,
+              responseType: "json"
+            }).
+            success(function(data){
+
+              var buses = data.Predictions;
+              var busesHeadingHome = [];
+
+              for (var x in buses) {
+                if (bus.routeId === buses[x].RouteID){
+                  // calculate arrival time
+                  var d1 = new Date ();
+                  var d2 = new Date ( d1 );
+                  d2.setMinutes ( d1.getMinutes() + buses[x].Minutes );
+
+                  var localTime = d2.toLocaleTimeString();
+                  var ampm = localTime.split(' ');
+
+                  var time = localTime.split(':');
+                  time[0]+':'+time[1]+' '+ampm[1]
+                  buses[x].ArrivalTime = time[0]+':'+time[1]+' '+ampm[1];
+
+                  busesHeadingHome.push(buses[x]);
+                }
+
+              }
+
+              $scope.busesHeadingHome = busesHeadingHome;
+              if (busesHeadingHome.length > 0){
+                $scope.no_buses_running_home = false;
+              }
+
+            }).
+            error(function(data){
+              console.log('Error: ' + data);
+            });
+          }
+
+          //execute
+          busWork();
+          busHome();
+          metroWork();
+          metroHome();
+
+          var pollBusHome;
+          var pollBusWork;
+          var pollMetroHome;
+          var pollMetroWork;
+          function pollWmata(){
+            var intervalTime = 10000; // 10 secs
+            pollBusHome = setInterval(busHome,intervalTime);
+            pollBusWork = setInterval(busWork,intervalTime);
+            pollMetroHome = setInterval(metroHome,intervalTime);
+            pollMetroWork = setInterval(metroWork,intervalTime);
+          }
+          pollWmata();
+
+
 
 
     }]);
