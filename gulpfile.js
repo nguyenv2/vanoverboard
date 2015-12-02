366b30f1c318f31d49853c519728f39a1422bc1e
Van Nguyen <van.nguyen@washpost.com>
2015-12-01 19:25:09 -0500

    more gulp madness

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,20 @@ var gutil = require('gulp-util');
 var coffee = require('gulp-coffee');
 var concat = require('gulp-concat');
 var browserify = require('gulp-browserify');
+var compass = require('gulp-compass');
 
 
 var coffeeSources = ['components/coffee/tagline.coffee'];
+
 var jsSources = [
   'app/scripts/*.js',
   'app/scripts/controllers/*.js',
   'app/scripts/controllers/apps/*.js',
 ];
+
+var sassSources = ['app/styles/main.scss'];
+
+
 
 gulp.task('default',function(){
 	console.log('this is gulp');
@@ -31,5 +37,21 @@ gulp.task('js', function() {
   gulp.src(jsSources)
     .pipe(concat('script.js'))
     .pipe(browserify())
-    .pipe(gulp.dest('app/scripts'));
+    .pipe(gulp.dest('target/scripts'));
 })
+
+gulp.task('compass', ['js'], function() {
+    gulp.src(sassSources)
+        .pipe(compass({
+            sass: 'app/styles/sass',
+            image: 'app/images',
+            style: 'expanded'
+        }))
+        .on('error',gutil.log)
+        .pipe(gulp.dest('target/styles'));
+})
+
+
+gulp.task('all', ['js','compass']);
+
+gulp.task('default', ['js','compass']);
